0419ebf3ec64a6063040cb596c302fb89d1ff99b
riley-s <53252351+riley-s@users.noreply.github.com>
2020-08-17 14:04:40 +1000

    test

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import './App.css';
+
+const App = () => (
+	<Fragment>
+		<h1>App</h1>
+	</Fragment>
+);
+
+export default App;
