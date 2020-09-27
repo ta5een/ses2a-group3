f990d9057d7252fd31f770452831bf653cf7e98f
tahscenery <tahscenery@gmail.com>
2020-09-28 01:55:17 +1000

    Minor changes

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 4000,
-  jwtSecret: process.env.JWT_SECRET || 'YOUR_secret_key',
-  mongoUri: process.env.MONGODB_URI || 'mongodb+srv://root:root123@study-group-webapp.dkasc.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || 'SECRET_TOKEN',
+  mongoUri: process.env.MONGODB_URI || 'mongodb+srv://root:root123@study-group-webapp.dkasc.mongodb.net/group-interest?retryWrites=true&w=majority',
 }
 
 export default config;
