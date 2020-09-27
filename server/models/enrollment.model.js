ce1a0ec630d2ea4061c917d1c9f64f8c433d9412
tahscenery <tahscenery@gmail.com>
2020-09-27 22:50:39 +1000

    Added models

---
+++ server/models/enrollment.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const EnrollmentSchema = new mongoose.Schema({
+  group: { type: mongoose.Schema.ObjectId, ref: 'Group' },
+  updated: Date,
+  enrolled: {
+    type: Date,
+    default: Date.now
+  },
+  member: { type: mongoose.Schema.ObjectId, ref: 'User' },
+  contentStatus: [
+    {
+      content: { type: mongoose.Schema.ObjectId, ref: 'Content' },
+      complete: Boolean
+    }
+  ],
+  completed: Date,
+});
+
+export default mongoose.model('Enrollment', EnrollmentSchema);
