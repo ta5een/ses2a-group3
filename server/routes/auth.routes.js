3d0f06f7996983c8f6c7b127931474ab32adff9d
tahscenery <tahscenery@gmail.com>
2020-10-01 22:57:53 +1000

    server/routes: Formatting

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,11 +1,14 @@
-import express from 'express'
-import authCtrl from '../controllers/auth.controller'
+import express from 'express';
+import authCtrl from '../controllers/auth.controller';
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/auth/signin')
-  .post(authCtrl.signin)
-router.route('/auth/signout')
-  .get(authCtrl.signout)
+router
+  .route('/auth/signin')
+  .post(authCtrl.signin);
 
-export default router
+router
+  .route('/auth/signout')
+  .get(authCtrl.signout);
+
+export default router;
--- server/routes/enrollment.routes.js
+++ server/routes/enrollment.routes.js
@@ -1,26 +1,30 @@
-import express from 'express';
-import enrollmentCtrl from '../controllers/enrollment.controller';
-import groupCtrl from '../controllers/group.controller';
 import authCtrl from '../controllers/auth.controller';
+import enrollmentCtrl from '../controllers/enrollment.controller';
+import express from 'express';
+import groupCtrl from '../controllers/group.controller';
 
 const router = express.Router();
 
-router.route('/api/enrollment/enrolled').get(authCtrl.requireSignin, enrollmentCtrl.listEnrolled);
+router
+  .route('/api/enrollment/enrolled')
+  .get(authCtrl.requireSignin, enrollmentCtrl.listEnrolled);
 
 router
-	.route('/api/enrollment/new/:groupId')
-	.post(authCtrl.requireSignin, enrollmentCtrl.findEnrollment, enrollmentCtrl.create);
-
-router.route('/api/enrollment/stats/:groupId').get(enrollmentCtrl.enrollmentStats);
+  .route('/api/enrollment/new/:groupId')
+  .post(authCtrl.requireSignin, enrollmentCtrl.findEnrollment, enrollmentCtrl.create);
 
 router
-	.route('/api/enrollment/complete/:enrollmentId')
-	.put(authCtrl.requireSignin, enrollmentCtrl.isMember, enrollmentCtrl.complete);
+  .route('/api/enrollment/stats/:groupId')
+  .get(enrollmentCtrl.enrollmentStats);
 
 router
-	.route('/api/enrollment/:enrollmentId')
-	.get(authCtrl.requireSignin, enrollmentCtrl.isMember, enrollmentCtrl.read)
-	.delete(authCtrl.requireSignin, enrollmentCtrl.isMember, enrollmentCtrl.remove);
+  .route('/api/enrollment/complete/:enrollmentId')
+  .put(authCtrl.requireSignin, enrollmentCtrl.isMember, enrollmentCtrl.complete);
+
+router
+  .route('/api/enrollment/:enrollmentId')
+  .get(authCtrl.requireSignin, enrollmentCtrl.isMember, enrollmentCtrl.read)
+  .delete(authCtrl.requireSignin, enrollmentCtrl.isMember, enrollmentCtrl.remove);
 
 router.param('groupId', groupCtrl.groupByID);
 router.param('enrollmentId', enrollmentCtrl.enrollmentByID);
