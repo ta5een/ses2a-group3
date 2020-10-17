f3ca05c3c9942a0da9bcd5fcd907ab982d89f5c0
tahscenery <tahscenery@gmail.com>
2020-10-18 01:56:23 +1100

    client: Removed Outcome test

--- packages/client/src/api/utils.ts
+++ packages/client/src/api/utils.ts
@@ -12,31 +12,3 @@ export async function handleResponse<T>(response: Response): Promise<T> {
     throw new Error(errorMessage);
   }
 }
-
-export type Success<T> = { type: "Success"; data: T };
-export type Error<E> = { type: "Error"; error: E };
-export type Outcome<T, E> = Success<T> | Error<E>;
-
-export function mapOutcomeSuccess<T, U, E>(
-  outcome: Outcome<T, E>,
-  onSuccess: (data: T) => U
-): Outcome<U, E> {
-  switch (outcome.type) {
-    case "Success":
-      return { type: "Success", data: onSuccess(outcome.data) };
-    default:
-      return outcome;
-  }
-}
-
-export function mapOutcomeError<T, E, F>(
-  outcome: Outcome<T, E>,
-  onError: (error: E) => F
-): Outcome<T, F> {
-  switch (outcome.type) {
-    case "Error":
-      return { type: "Error", error: onError(outcome.error) };
-    default:
-      return outcome;
-  }
-}
